9efc01579191ff293b727baf18a77d871e128010
Om <ommayekar19@gmail.com>
2026-02-18 22:05:22 +0530

    review app launch

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import *
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import time
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+	if request.method == "POST":
+		file = request.files["file"]
+		data = file.read().decode("utf-8")
+		lines = data.splitlines()[1:]
+		sia = SentimentIntensityAnalyzer()
+		result = "review,score,label\n"
+		for line in lines:
+			ps = sia.polarity_scores(line.strip())
+			if ps["compound"] >= 0.05:
+				label="Positive"
+			elif ps["compound"] <= -0.05:
+				label="Negative"
+			else:
+				label = "Neutral"
+			result += line + "," +str(ps["compound"]) + "," + label + "\n"
+		
+		with open("output.csv", "w") as f:
+			f.write(result)
+		time.sleep(5)
+		return send_file("output.csv", as_attachment=True)
+	else:
+		return render_template("home.html")
+
+#app.run(debug=True, use_reloader=True, port=8081)
